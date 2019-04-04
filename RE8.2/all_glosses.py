adb02c1b37ad336ae41361ab8ecae0e2b2d53987
Charles Zhang <charleszhang99@yahoo.com>
2019-04-04 15:36:11 -0700

    Forgotten files

---
+++ RE8.2/all_glosses.py
@@ -0,0 +1,10 @@
+import utils
+import read
+from argparser import args, need_to_compare, project_dir
+
+settings = read.read_settings_file(f'{project_dir}/{args.project}.parameters.xml',
+                                   mel=args.mel,
+                                   recon=args.recon)
+
+for gloss in utils.all_glosses(read.read_attested_lexicons(settings)):
+    print(gloss)
