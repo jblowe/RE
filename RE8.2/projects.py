f0183328aba73a618250ee4fc09d4269da2566f5
Charles Zhang <charleszhang99@yahoo.com>
2019-04-04 08:55:24 -0700

    Fix

--- RE8.2/projects.py
+++ RE8.2/projects.py
@@ -1,6 +1,6 @@
 import os
 
-project_list = {'TGTM': '../RE7/DATA/TGTM',
+projects = {'TGTM': '../RE7/DATA/TGTM',
                 'ROMANCE': '../RE7/DATA/ROMANCE',
                 'DEMO93': '../RE7/DATA/DEMO93',
                 'POLYNESIAN': '../RE7/DATA/POLYNESIAN',
