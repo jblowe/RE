ea98574607d9b2d1fb37a3c30aa0dd5266bc076d
Charles Zhang <charleszhang99@yahoo.com>
2019-04-19 21:12:35 -0700

    Do not load hooks when there are none.

--- RE8.2/load_hooks.py
+++ RE8.2/load_hooks.py
@@ -6,4 +6,7 @@ def load_hook(project):
     base_dir = projects.projects[project]
     sys.path.append('toolbox/')
     sys.path.append(base_dir)
-    import hook
+    try:
+        import hook
+    except ModuleNotFoundError:
+        print('No preprocessing needed.')
