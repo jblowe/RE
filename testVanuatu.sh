3c2c01c1395cbb5af8a7c999ffc66e1c835d9d3a
John Lowe <jblowe@berkeley.edu>
2019-04-27 14:06:00 -0700

    we shouldn't have to clone the toolbox manually

--- testVanuatu.sh
+++ testVanuatu.sh
@@ -2,7 +2,6 @@ set -x
 
 pwd
 
-git clone https://github.com/goodmami/toolbox.git
 python3 REcli.py VANUATU --mel none --mel2 clics
 echo 'No mel to gold stats'
 head ../projects/VANUATU/VANUATU.default.analysis.txt
