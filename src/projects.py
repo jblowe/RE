a4849bd0d24d39d6141aba68aa923cb6cf92bc79
John Lowe <jblowe@berkeley.edu>
2019-04-27 19:45:11 -0700

    added LOLOISH to list of projects

--- src/projects.py
+++ src/projects.py
@@ -4,6 +4,7 @@ projects = {'TGTM': '../projects/TGTM',
             'ROMANCE': '../projects/ROMANCE',
             'DEMO93': '../projects/DEMO93',
             'POLYNESIAN': '../projects/POLYNESIAN',
+            'LOLOISH': '../projects/LOLOISH',
             'GERMANIC': '../projects/GERMANIC',
             'VANUATU': '../projects/VANUATU'
 }
