b38c67b1b292b9011b18943ad74cada896f62d3f
John Lowe <jblowe@berkeley.edu>
2019-04-27 19:44:44 -0700

    csv_to_lexicon.py creates RE-style XML from .csv files; fixed csv_to_re.py

---
+++ src/csv_to_lexicon.py
@@ -0,0 +1,21 @@
+import read
+import sys
+import os
+import RE
+import csv
+import serialize
+
+base_dir = os.path.dirname(sys.argv[1])
+
+with open(sys.argv[1], 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter='\t')
+    # get first row (header)
+    for row in reader:
+        header = row
+        break
+x = sys.argv[2]
+table_layout = [int(x) for x in sys.argv[2].split(',')]
+
+lexicons = read.read_tabular_lexicons(sys.argv[1], table_layout)
+
+serialize.serialize_lexicons(lexicons, base_dir)
--- src/csv_to_re.py
+++ src/csv_to_re.py
@@ -1,10 +1,20 @@
 import read
 import sys
+import RE
+import csv
 
 # insert list of language names here
-languages = []
-table = read.read_csv_correspondences(sys.argv[0], 'placeholder', languages)
+
+with open(sys.argv[1], 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter='\t')
+    # get first row (header)
+    for row in reader:
+        languages = row[5:]
+        break
+
+
+table = read.read_csv_correspondences(sys.argv[1], 'placeholder', languages)
 
 RE.Parameters(table,
               RE.SyllableCanon({}, 'placeholder', []),
-              'placeholder', None).serialize('converted.xml')
+              'placeholder', None).serialize(sys.argv[2])
