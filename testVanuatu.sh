449e7f6736f587502284f4de772c748af1d2e6b4
John Lowe <jblowe@berkeley.edu>
2019-04-29 16:19:08 -0700

    small clarification of test run output

--- testVanuatu.sh
+++ testVanuatu.sh
@@ -5,5 +5,5 @@ pwd
 git submodule update --init --recursive
 
 time python3 REcli.py VANUATU --mel none --mel2 clics
-echo 'No mel to gold stats'
+echo 'Comparing No-MEL to gold stats'
 head ../projects/VANUATU/VANUATU.default.analysis.txt
