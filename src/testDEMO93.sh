7aa17fc2bdda174733e1441bd39950efc616ceef
John Lowe <jblowe@berkeley.edu>
2019-04-30 20:32:34 -0700

    TGTM test script only test set creation, DEMO93 does both set creation and MEL comparison

--- src/testDEMO93.sh
+++ src/testDEMO93.sh
@@ -1,6 +1,9 @@
 set -x
 
 pwd
+# first make sets
+time python3 REcli.py DEMO93 --mel hand
+# second test mel comparison
 time python3 REcli.py DEMO93 --mel hand --mel2 none
 echo 'Comparing No-MEL to gold stats'
 head ../projects/DEMO93/DEMO93.default.analysis.txt
