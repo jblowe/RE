0201d2690bc1807ee3065c0ed19c8b985d0e9ea0
John Lowe <jblowe@berkeley.edu>
2019-05-06 18:57:33 -0700

    cleanup.sh now leaves statistics files where they were

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
-for d in ../projects; do git clean -fd ../projects/$d  ; done
+for d in ../projects; do git clean -fd ../projects/$d -e *.statistics.txt ; done
 git checkout -- ../projects/TGTM/TGTM.*.data.xml
 git checkout -- ../projects/POLYNESIAN/*.data.xml
