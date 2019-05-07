d75d44543acf2c82ec1ffc52dd8f69bdc7bcb2b5
John Lowe <jblowe@berkeley.edu>
2019-05-06 17:51:57 -0700

    repair TGTM test script

--- testTGTM.sh
+++ testTGTM.sh
@@ -4,7 +4,6 @@ DATE=`date +%Y-%m-%d-%H-%M`
 
 pwd
 # only create "best sets" (i.e. using mel)
-DATE=`date +%Y-%m-%d-%H-%M`
-time python3 REcli.py TGTM --mel hand > ../projects/TGTM/TGTM.${DATE}.${DATE}.statistics.txt
-cat ../projects/TGTM/TGTM.${DATE}.${DATE}.statistics.txt
+time python3 REcli.py TGTM --mel hand > ../projects/TGTM/TGTM.${DATE}.statistics.txt
+cat ../projects/TGTM/TGTM.${DATE}.statistics.txt
 
