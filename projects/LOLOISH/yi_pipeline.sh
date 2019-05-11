499c907dcac1c2791289f1b6175e3476753aeeb2
John Lowe <jblowe@berkeley.edu>
2019-05-11 08:21:42 -0700

    corrections and improvements to Yi data

--- projects/LOLOISH/yi_pipeline.sh
+++ projects/LOLOISH/yi_pipeline.sh
@@ -6,6 +6,12 @@ set -x
 python3 ../../src/csv_to_lexicon.py NYI.TAB.u8.csv "1,0, 2"
 python3 ../../src/csv_to_lexicon.py SYI.TAB.u8.csv "1,0, 2"
 
+# other minor fixups
+#
+# remove parens surrounding forms
+perl -i -pe 's#<hw>\((.*)\)</hw>#<hw>\1</hw>#;' *.data.xml
+perl -i -pe 's#<hw>(.*?), (.*?)</hw>#<hw>\1</hw>\n    <hw>\2</hw>#;' *.data.xml
+
 # run the csv converter for the correspondences
 python3 ../../src/csv_to_re.py NYI.TOC.u8.csv NYI.correspondences.xml NYI.classes.xml
 python3 ../../src/csv_to_re.py SYI.TOC.u8.csv SYI.correspondences.xml SYI.classes.xml
