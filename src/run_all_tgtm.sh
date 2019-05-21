cd312e3380356b1934729bd682f20cc0b889f423
John Lowe <jblowe@berkeley.edu>
2019-05-21 05:44:35 -0700

    corrected version of this script

--- src/run_all_tgtm.sh
+++ src/run_all_tgtm.sh
@@ -5,19 +5,19 @@ pwd
 python3 REcli.py TGTM --mel hand --mel2 none
 [ $? -ne 0 ] && exit 1;
 echo 'No mel to gold stats'
-head ..data/TGTM/TGTM.default.analysis.txt
+head ../projects/TGTM/TGTM.default.analysis.txt
 
 python3 REcli.py TGTM --mel hand --mel2 wordnet
 [ $? -ne 0 ] && exit 1;
 echo 'WordNet to gold stats'
-head ..data/TGTM/TGTM.default.analysis.txt
+head ../projects/TGTM/TGTM.default.analysis.txt
 
 python3 REcli.py TGTM --mel hand --mel2 wordnet-pairs
 [ $? -ne 0 ] && exit 1;
 echo 'WordNet pairs to gold stats'
-head ..data/TGTM/TGTM.default.analysis.txt
+head ../projects/TGTM/TGTM.default.analysis.txt
 
 python3 REcli.py TGTM --mel hand --mel2 clics
 [ $? -ne 0 ] && exit 1;
 echo 'Clics to gold stats'
-head ..data/TGTM/TGTM.default.analysis.txt
+head ../projects/TGTM/TGTM.default.analysis.txt
