18b666496b43e5592ca3b3003ac684fd25028fdc
Charles Zhang <charleszhang99@yahoo.com>
2019-05-28 23:39:05 -0700

    Autogenerate correspondences from Alex's table.

--- hook.py
+++ hook.py
@@ -4,11 +4,15 @@ import toolbox
 import sys
 import os
 import xml.etree.ElementTree as ET
+import RE
+import read
+import csv
 from collections import defaultdict
 from xml.dom import minidom
 
 base_dir = os.path.dirname(__file__)
 filename = os.path.join(base_dir, 'AlexF_NorthVan-reconstructions_01.txt')
+correspondence_filename = os.path.join(base_dir, 'AlexF_Vanuatu-correspondences.csv')
 
 languages = ['hiw', 'ltg', 'lhi', 'lyp', 'vlw', 'mtp', 'lmg', 'vra', 'vrs', 'msn', 'mta', 'num', 'drg', 'kro', 'olr', 'lkn', 'mrl']
 
@@ -36,6 +40,50 @@ def read_toolbox_file():
                 i += 1
             mels[set_number] = mel
     return (zipped, mels)
+
+def syllable_type(proto):
+    if proto in ['a', 'o', 'i', 'e', 'u']:
+        return 'V'
+    else:
+        return 'C'
+
+def context_from(string):
+    if '_' not in string:
+        return (None, None)
+    (left_context, right_context) = string.split('_')
+    # We have to punt because we cannot handle these contexts as they
+    # are now.
+    if 'C' in left_context or 'V' in left_context or 'ˈ' in left_context:
+        left_context = None
+    if 'C' in right_context or 'V' in right_context or 'ˈ' in right_context:
+        right_context = None
+    if left_context:
+        left_context = left_context.split('/')
+    if right_context:
+        right_context = right_context.split('/')
+    return (left_context, right_context)
+
+def read_vanuatu_csv(filename):
+    table = RE.TableOfCorrespondences('VANUATU', languages)
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        # element of redundancy here, but we can't assume order
+        skipped = read.skip_comments(reader)
+        names = next(skipped)[5:]
+        print(names)
+        for (number, row) in enumerate(skipped):
+            print(row)
+            table.add_correspondence(RE.Correspondence(
+                str(number), context_from(row[2]), syllable_type(row[1]), row[1],
+                dict(zip(names, (x.split('|') for x in row[5:])))))
+    return table
+
+RE.Parameters(read_vanuatu_csv(correspondence_filename),
+              RE.SyllableCanon({}, '(CV)+', []),
+              'pnv', None).serialize(
+                  os.path.join(base_dir, 'VANUATU.correspondences.xml'))
+
+print('made correspondence xml')
 
 def xml_name(x):
     return x + '.data.xml'
