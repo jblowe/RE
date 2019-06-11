a8b7f399f76be49ba1ea4929a7f05b6664afb8a2
John Lowe <jblowe@berkeley.edu>
2019-06-11 10:51:35 -0700

    revise Wordnet MEL creation script

--- semantic_sources/wnlookup.py
+++ semantic_sources/wnlookup.py
@@ -6,34 +6,52 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
 root = ET.Element('semantics')
-counter = 0
+count_found = 0
+count_not_found = 0
+not_found = []
 
-with open('wn.not_found', 'w') as not_found:
-    for line in sys.stdin:
-        line = line.strip()
-        gloss = line.split('\t')[0]
-        orig = gloss
-        gloss = gloss.split('/')[0]
-        gloss = gloss.split(' (')[0]
-        gloss = gloss.replace('be ','')
-        gloss = gloss.replace('go ','')
-        gloss = gloss.replace('être ','')
-        gloss = gloss.replace(' ','_')
-        #print('{0}\t{1}'.format(line, wn.synsets(gloss)))
-        lemmas = [l.lemmas() for l in wn.synsets(gloss)]
-        if len(lemmas) == 0:
-            lemmas = [l.lemmas() for l in wn.synsets(gloss, lang='fra')]
-        if lemmas == []:
-            not_found.write('{} \n'.format(orig))
-        synonyms = {}
-        for lemma in lemmas:
-            for l in lemma:
-                synonyms[l.name()] = 1
-        for synonym in synonyms:
-            counter += 1
-            mel = ET.SubElement(root, 'mel', id="wn" + str(counter))
-            ET.SubElement(mel, 'gl').text = gloss
-            ET.SubElement(mel, 'gl').text = synonym
+for line in sys.stdin:
+    line = line.strip()
+    gloss = line.split('\t')[0]
+    orig = gloss
+    gloss = gloss.split('/')[0]
+    gloss = gloss.split(' (')[0]
+    gloss = gloss.replace('be ','')
+    gloss = gloss.replace('go ','')
+    gloss = gloss.replace('être ','')
+    gloss = gloss.replace(' ','_')
+    #print('{0}\t{1}'.format(line, wn.synsets(gloss)))
+    lemmas = [l.lemmas() for l in wn.synsets(gloss)]
+    if len(lemmas) == 0:
+        lemmas = [l.lemmas() for l in wn.synsets(gloss, lang='fra')]
+    if lemmas == []:
+        #not_found.write('{} \n'.format(orig))
+        count_not_found += 1
+        not_found.append(gloss)
+        continue
+    synonyms = {}
+    for lemma in lemmas:
+        for l in lemma:
+            synonyms[l.name().replace('_',' ')] = 1
+    count_found += 1
+    mel = ET.SubElement(root, 'mel', id="wn" + str(count_found))
+    pivot_set = False
+    for synonym in sorted(synonyms):
+        sub = ET.SubElement(mel, 'gl')
+        sub.text = synonym
+        if gloss == synonym:
+            sub.set('pivot', 'samelg')
+            pivot_set = True
+    if not pivot_set:
+        sub = ET.SubElement(mel, 'gl')
+        sub.text = gloss
+        sub.set('pivot', 'otherlg')
+for gloss in sorted(not_found):
+    count_found += 1
+    mel = ET.SubElement(root, 'mel', id="wn" + str(count_found))
+    sub = ET.SubElement(mel, 'gl')
+    sub.text = gloss.replace('_',' ')
+    sub.set('pivot', 'notfound')
 
 sys.stdout.write(minidom.parseString(ET.tostring(root)).toprettyxml(indent='   '))
         
