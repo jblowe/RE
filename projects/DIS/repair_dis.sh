13a4c82ac364e576a43822e028d34451a6f58441
John Lowe <jblowe@berkeley.edu>
2019-06-11 04:43:55 -0700

    initial revised TGTM 'DIS' files: unicode, character hacks handled, etc.

---
+++ projects/DIS/repair_dis.sh
@@ -0,0 +1,26 @@
+git checkout -- *.CLS
+git checkout -- *.DIS
+git checkout -- *.TOC
+
+for EXT in DIS TOC CLS
+do
+  perl -i -pe 's/\\/#/g' *.${EXT}
+  perl -i -pe 's/\r//g'  *.${EXT}
+  perl -i -pe 's/`/#/g'  *.${EXT}
+
+  perl -i -pe 's/\x1A//g' *.${EXT}
+done
+
+perl ../STEDT5-U4Map-7.plx .
+
+for f in `ls *.u8`
+do
+  python ../RE_DATA_1994/fix_sequences.py $f $f.fix
+done
+
+for f in `ls *.DIS *.TOC *.CLS`
+do
+  cp $f.u8.fix $f
+  rm $f.u8 $f.u8.fix
+done
+
