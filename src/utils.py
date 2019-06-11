3dc238e35c1c918d80b6758882cb096787cc6ebe
John Lowe <jblowe@berkeley.edu>
2019-06-11 06:09:45 -0700

    fix coverage computations to check glosses by individual lexicon

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,17 @@
 # Misc. utils
-def all_glosses(lexicon):
+def all_glosses(lexicons):
     glosses = set()
-    for language in lexicon:
-        for form in lexicon[language].forms:
+    for language in lexicons:
+        for form in lexicons[language].forms:
             if form.gloss:
                 glosses.add(form.gloss)
     return glosses
+
+
+def glosses_by_language(single_lexicon):
+    glosses = set()
+    for form in single_lexicon.forms:
+        if form.gloss:
+            glosses.add(form.gloss)
+    return glosses
+
