eb2eacc5a0ae2751608ef9845b0830754b79362b
Charles Zhang <charleszhang99@yahoo.com>
2019-06-13 21:21:46 -0700

    Revert "skip sets with 'no mels', unless there are no mels at all"
    This reverts commit f40b6033949e11c78733a39723b7f82586b2abef.
    
    We will need a more flexible approach to handle hiding mels. For now,
    do not hide such information from the linguist.

--- src/RE.py
+++ src/RE.py
@@ -317,8 +317,7 @@ def create_sets(projections, statistics, mels, root=True):
         for supporting_form in support:
             if isinstance(supporting_form, ModernForm):
                 for associated_mel in mel.associated_mels(mels, supporting_form.gloss):
-                    if associated_mel.id != '' or mels is None:
-                        distinct_mels[associated_mel].append(supporting_form)
+                    distinct_mels[associated_mel].append(supporting_form)
             else:
                 distinct_mels[mel.default_mel].append(supporting_form)
         for distinct_mel, support in distinct_mels.items():
