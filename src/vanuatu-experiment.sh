66421045c88df452e38f1bc6344963c3168d02fc
John Lowe <jblowe@berkeley.edu>
2019-06-22 12:45:47 -0700

    revised 'Vanuatu experiment'. should work now.

--- src/vanuatu-experiment.sh
+++ src/vanuatu-experiment.sh
@@ -3,8 +3,13 @@ set -x
 ./cleanup.sh
 git checkout -- ../projects/VANUATU/*
 
+ls -l ././projects/VANUATU
+
 # make a version of the code with the revised disyllabic canon
-perl -pe 's/CV\+/(C)V((C)V)+/' ../projects/VANUATU/hook.py > ../projects/VANUATU/ CVCVhook.py
+perl -pe 's/\(CV\)./(C)V((C)V)+/' ../projects/VANUATU/hook.py > ../projects/VANUATU/CVCVhook.py
+
+# make a version of the correspondences which has *p,*w > zero
+perl -pe 's/([^\d],)([wp]),/\1\2|∅,/g if /,[wp],,,,/' ../projects/VANUATU/VANUATU.experimental.correspondences.csv > ../projects/VANUATU/VANUATU.pwzeroexperimental.correspondences.csv
 
 # here are our two different ToCs
 diff ../projects/VANUATU/VANUATU.pwzeroexperimental.correspondences.csv ../projects/VANUATU/VANUATU.experimental.correspondences.csv
@@ -12,7 +17,10 @@ diff ../projects/VANUATU/VANUATU.pwzeroexperimental.correspondences.csv ../proje
 # here are our two different Syllable canons
 grep RE.SyllableCanon ../projects/VANUATU/*hook.py
 
-# OK run the status quo, using Alex's "*p,*w without zero" version of ToC, call it "CV"
+# run the status quo, using Alex's "*p,*w without zero" version of ToC, without "parsimonious", call it "hand"
+time python3 REcli.py VANUATU -r hand --mel hand > /dev/null
+
+# OK run the same thing, with parsimonious, call it "CV"
 time python3 REcli.py VANUATU -w -r CV --mel hand > /dev/null
 
 # use the revised canon, call the run "CVCV"
@@ -20,12 +28,15 @@ cp ../projects/VANUATU/CVCVhook.py ../projects/VANUATU/hook.py
 time python3 REcli.py VANUATU -w -r CVCV --mel hand  > /dev/null
 
 # use JB's "revised" ToC (*p,*w can > zero), call it CVCVpw
-cp VANUATU.pwzeroexperimental.correspondences.csv VANUATU.experimental.correspondences.csv
+cp ../projects/VANUATU/VANUATU.pwzeroexperimental.correspondences.csv ../projects/VANUATU/VANUATU.experimental.correspondences.csv
 time python3 REcli.py VANUATU -w -r CVCVpw --mel hand  > /dev/null
 
 # restrore the original canon, try again call it CVpw
 git checkout -- ../projects/VANUATU/hook.py
 time python3 REcli.py VANUATU -w -r CVpw --mel hand  > /dev/null
+
+# restrore the original ToC
+git checkout -- ../projects/VANUATU/VANUATU.experimental.correspondences.csv
 
 # generate comparisons
 python3 REcli.py -x -- VANUATU
