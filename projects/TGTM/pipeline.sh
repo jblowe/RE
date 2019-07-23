922a8ace071b65d07aca3acf898ff1f813ce21dd
John Lowe <jblowe@berkeley.edu>
2019-07-23 06:59:37 -0700

    TGTM pipeline needed a bit of reorganization

--- projects/TGTM/pipeline.sh
+++ projects/TGTM/pipeline.sh
@@ -18,6 +18,19 @@ perl Lex2XML.pl ALLTHAK_1993.DAT TEMP.tuk.data.xml tuk.xml.log tuk
 #perl Lex2XML.pl MONTAG_1994.DAT TEMP.tag2.data.xml tag.xml.log tag
 #perl Lex2XML.pl MONTUK_1991.DAT TEMP.tuk2.data.xml tuk.xml.log tuk
 
+# apply xslt
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.gha.data.xml > TGTM.gha.data.xml 2> gha.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.mar.data.xml > TGTM.mar.data.xml 2> mar.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.pra.data.xml > TGTM.pra.data.xml 2> pra.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.ris.data.xml > TGTM.ris.data.xml 2> ris.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.sahu.data.xml > TGTM.sahu.data.xml 2> sahu.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.syang.data.xml > TGTM.syang.data.xml 2> syang.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tag.data.xml > TGTM.tag.data.xml 2> tag.xslt.log
+python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tuk.data.xml > TGTM.tuk.data.xml 2> tuk.xslt.log
+
+#python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tag2.data.xml > TGTM.tag2.data.xml 2> tag2.xslt.log
+#python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tuk2.data.xml > TGTM.tuk2.data.xml 2> tuk2.xslt.log
+
 # other minor fixups
 #perl -i -p addngl.pl TEMP.*.data.xml
 perl -i -p addngl.pl TEMP.gha.data.xml
@@ -32,34 +45,19 @@ perl -i -p addngl.pl TEMP.tuk.data.xml
 #perl -i -p addngl.pl TEMP.tag2.data.xml
 #perl -i -p addngl.pl TEMP.tuk2.data.xml
 
-# apply xslt
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.gha.data.xml > TGTM.gha.data.xml 2> gha.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.mar.data.xml > TGTM.mar.data.xml 2> mar.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.pra.data.xml > TGTM.pra.data.xml 2> pra.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.ris.data.xml > TGTM.ris.data.xml 2> ris.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.sahu.data.xml > TGTM.sahu.data.xml 2> sahu.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.syang.data.xml > TGTM.syang.data.xml 2> syang.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tag.data.xml > TGTM.tag.data.xml 2> tag.xslt.log
-python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tuk.data.xml > TGTM.tuk.data.xml 2> tuk.xslt.log
-
-#python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tag2.data.xml > TGTM.tag2.data.xml 2> tag2.xslt.log
-#python $1/xsltproc.py $1/../styles/fmtLex.xsl TEMP.tuk2.data.xml > TGTM.tuk2.data.xml 2> tuk2.xslt.log
-
-
 #exit
-
-cat *.xslt.log > all.logs
-cat all.logs
 
 ls -l
 
 # combine the conversion logs (they have useful stats) into one
-cat *.xml.log > TGTM.xml.log
+cat *.xml.log > all.xml.logs
 rm *.xml.log
 
 # combine the xslt logs into one
-cat *.xslt.log > TGTM.xslt.log
+cat *.xslt.log > all.xslt.logs
 rm *.xslt.log
 
+python3 -c 'import os, glob;list(map(os.remove, glob.glob("*.xslt.log")))'
 # get rid of the temp files
-rm TEMP.*.data.xml
+python3 -c 'import os, glob;list(map(os.remove, glob.glob("TEMP.*.xml")))'
+
