b62a38c2074edec6cef043c837872403ec496235
John Lowe <jblowe@berkeley.edu>
2019-08-04 07:50:17 -0700

    REwww's 'run' function now uses the new REcli syntax

--- run_make.py
+++ run_make.py
@@ -4,28 +4,61 @@ import time
 
 
 def make(project, experiment, parameters):
+    messages = []
     try:
         elapsed_time = time.time()
         if project == 'ALL':
-            return f'make ALL is disabled for now.'
+            messages.append(f'make ALL is disabled for now.')
+            return messages
             os.chdir('..')
             p_object = subprocess.call(['git', 'pull', '-v'])
             p_object = subprocess.call(['make', '-w'])
             os.chdir('REwww')
         else:
-            cli = [os.path.join(project, experiment)]
+            cli = []
             if 'name' in parameters: cli.append(f"-r{parameters['name']}")
             if 'mel' in parameters: cli.append(f"-m{parameters['mel']}" )
             if parameters['strict'] == 'yes': cli.append('-w')
+            # REcli.py compare TGTM experiment1 experiment1 --run1 with-hand --run2 with-clics
+            cli = [project, experiment]
             os.chdir(os.path.join('..', 'src'))
             # p_object = subprocess.call(['git', 'pull', '-v'])
-            print(['python3', 'REcli.py'] + cli)
-            p_object = subprocess.call(['python3', 'REcli.py'] + cli)
-            # (re)run compare
-            p_object = subprocess.call(['python3', 'REcli.py', cli[0], '-x'])
+            messages.append(' '.join(['python3', 'REcli.py', 'run',] + cli))
+            p_object = subprocess.call(['python3', 'REcli.py', 'run'] + cli)
             os.chdir(os.path.join('..', 'REwww'))
         elapsed_time = time.time() - elapsed_time
         return '{:.2f}'.format(elapsed_time), '(Re)run succeeded', True
     except:
-        raise
+        return '{:.2f}'.format(elapsed_time), 'Upstream run failed.', False
+
+
+def compare(project, experiment1, experiment2, run1, run2):
+    messages = []
+    try:
+        elapsed_time = time.time()
+        if project == 'ALL':
+            messages.append(f'make ALL is disabled for now.')
+            return messages
+            os.chdir('..')
+            p_object = subprocess.call(['git', 'pull', '-v'])
+            p_object = subprocess.call(['make', '-w'])
+            os.chdir('REwww')
+        else:
+            cli = []
+            if 'name' in parameters: cli.append(f"-r{parameters['name']}")
+            if 'mel' in parameters: cli.append(f"-m{parameters['mel']}" )
+            if parameters['strict'] == 'yes': cli.append('-w')
+            # REcli.py compare TGTM experiment1 experiment1 --run1 with-hand --run2 with-clics
+            cli = [project, experiment]
+            os.chdir(os.path.join('..', 'src'))
+            # p_object = subprocess.call(['git', 'pull', '-v'])
+            messages.append(' '.join(['python3', 'REcli.py', 'run',] + cli))
+            p_object = subprocess.call(['python3', 'REcli.py', 'run'] + cli)
+            # (re)run IR compare
+            messages.append(' '.join(['python3', 'REcli.py', 'run'] + cli))
+            # p_object = subprocess.call(['python3', 'run', 'REcli.py', 'compare', cli[0], cli[0], '-x'])
+            os.chdir(os.path.join('..', 'REwww'))
+        elapsed_time = time.time() - elapsed_time
+        return '{:.2f}'.format(elapsed_time), '(Re)run succeeded', True
+    except:
         return '{:.2f}'.format(elapsed_time), 'Upstream run failed.', False
