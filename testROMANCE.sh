2b94dc08049a3114c7a083df91db93aa234a5ed9
John Lowe <jblowe@berkeley.edu>
2019-08-04 07:54:22 -0700

    revise testROMANCE.sh to use new REcli syntax

--- testROMANCE.sh
+++ testROMANCE.sh
@@ -4,19 +4,19 @@ DATE=`date +%Y-%m-%d-%H-%M`
 
 pwd
 # first make sets
-#time python3 REcli.py --run tree -t tree ROMANCE/experiments/semantics > ../projects/ROMANCE/experiments/semantics/ROMANCE.${DATE}.tree.statistics.txt
+#time python3 REcli.py --run tree -t tree ROMANCE semantics > ../experiments/ROMANCE/semantics/ROMANCE.${DATE}.tree.statistics.txt
 #[ $? -ne 0 ] && exit 1;
-#cat ../projects/ROMANCE/experiments/semantics/ROMANCE.${DATE}.tree.statistics.txt
+#cat ../experiments/ROMANCE/semantics/ROMANCE.${DATE}.tree.statistics.txt
 
 # second make strict sets
-time python3 REcli.py -w --run tree-strict -t tree ROMANCE/experiments/semantics > ../projects/ROMANCE/experiments/semantics/ROMANCE.${DATE}.tree.statistics.txt
+time python3 REcli.py run ROMANCE semantics -w --run tree-strict -t tree > ../experiments/ROMANCE/semantics/ROMANCE.${DATE}.tree.statistics.txt
 [ $? -ne 0 ] && exit 1;
-cat ../projects/ROMANCE/experiments/semantics/ROMANCE.${DATE}.tree.statistics.txt
+cat ../experiments/ROMANCE/semantics/ROMANCE.${DATE}.tree.statistics.txt
 
 # coverage
-python3 REcli.py -c -m hand ROMANCE/experiments/semantics > ../projects/ROMANCE/experiments/semantics/ROMANCE.mel.coverage.txt
+python3 REcli.py coverage ROMANCE semantics hand > ../experiments/ROMANCE/semantics/ROMANCE.mel.coverage.txt
 
 # compare results of all runs
-python3 REcli.py -x -- ROMANCE/experiments/semantics
+#python3 REcli.py compare ROMANCE semantics
 
 exit 1
