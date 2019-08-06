f5ad21ef6ebab2501a90a3068ce93e83715704b2
John Lowe <jblowe@berkeley.edu>
2019-08-06 11:33:39 -0700

    revised testLOLOISH.sh for new cli conventions

--- testLOLOISH.sh
+++ testLOLOISH.sh
@@ -4,44 +4,34 @@ DATE=`date +%Y-%m-%d-%H-%M`
 
 XPWD=`pwd`
 # run the pipeline first to set up the data
-cd ../projects/LOLOISH/
-cp * experiments/semantics
-cd experiments/semantics; ./pipeline.sh
+rm -rf ../experiments/SYI
+rm -rf ../experiments/NYI
+mkdir ../experiments/SYI
+mkdir ../experiments/NYI
+cp -r ../projects/LOLOISH ../experiments/SYI/semantics
+cp -r ../projects/LOLOISH ../experiments/NYI/semantics
+cd ../experiments/SYI/semantics; ./pipeline.sh
+cd ../../NYI/semantics         ; ./pipeline.sh
 [ $? -ne 0 ] && exit 1;
-
-# make the "standard" sets -- with the MEL
 
 cd $XPWD
 
-# generate the strict sets
-time python3 REcli.py NYI/experiments/semantics -w --run hand-strict --mel hand
-time python3 REcli.py SYI/experiments/semantics -w --run hand-strict --mel hand
-
-# do the comparison of MEL with no-mel
-time python3 REcli.py NYI/experiments/semantics --mel hand --mel2 none
-[ $? -ne 0 ] && exit 1;
-echo 'Comparing Gold to No-MEL stats...'
-head ../projects/LOLOISH/experiments/semantics/NYI.default.analysis.txt
-echo 'Comparing Gold to No-MEL stats...'
-time python3 REcli.py SYI/experiments/semantics --mel hand --mel2 none
-[ $? -ne 0 ] && exit 1;
-head ../projects/LOLOISH/experiments/semantics/SYI.default.analysis.txt
-
 # make the "standard" sets -- with the MEL
-time python3 REcli.py NYI/experiments/semantics -r hand --mel hand  > ../projects/LOLOISH/experiments/semantics/NYI.${DATE}.statistics.txt
-[ $? -ne 0 ] && exit 1;
-cat ../projects/LOLOISH/experiments/semantics/NYI.${DATE}.statistics.txt
+time python3 REcli.py upstream NYI semantics    --run hand        --mel hand
+time python3 REcli.py upstream SYI semantics    --run hand        --mel hand
 
-time python3 REcli.py SYI/experiments/semantics -r hand --mel hand  > ../projects/LOLOISH/experiments/semantics/SYI.${DATE}.statistics.txt
-[ $? -ne 0 ] && exit 1;
-cat ../projects/LOLOISH/experiments/semantics/SYI.${DATE}.statistics.txt
-
-# coverage
-python3 REcli.py -c -m hand SYI/experiments/semantics > ../projects/SYI/SYI.mel.coverage.txt
-python3 REcli.py -c -m hand NYI/experiments/semantics > ../projects/NYI/NYI.mel.coverage.txt
+# generate the strict sets
+time python3 REcli.py upstream NYI semantics -w --run hand-strict --mel hand
+time python3 REcli.py upstream SYI semantics -w --run hand-strict --mel hand
 
 # compare
-python3 REcli.py -x -- SYI/experiments/semantics
-python3 REcli.py -x -- NYI/experiments/semantics
+time python3 REcli.py compare NYI semantics -r1 hand-strict semantics -r2 hand > dev/null
+time python3 REcli.py compare SYI semantics -r1 hand-strict semantics -r2 hand > dev/null
 
-exit 1
+# coverage
+time python3 REcli.py coverage NYI semantics hand
+[ $? -ne 0 ] && exit 1;
+time python3 REcli.py coverage SYI semantics hand
+[ $? -ne 0 ] && exit 1;
+
+exit 0
