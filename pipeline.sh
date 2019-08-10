f31a50c30f7a3c651c75250e8f8c8adc62fe545a
John Lowe <jblowe@berkeley.edu>
2019-08-10 06:14:58 -0700

    pipelines now all refer to 'python' as the python to use

--- pipeline.sh
+++ pipeline.sh
@@ -3,9 +3,9 @@
 set -x
 
 # run the csv converter for the data
-python3 ../../../src/csv_to_lexicon.py NYI.TAB.u8.csv "1,0, 2"
+python ../../../src/csv_to_lexicon.py NYI.TAB.u8.csv "1,0, 2"
 [ $? -ne 0 ] && exit 1;
-python3 ../../../src/csv_to_lexicon.py SYI.TAB.u8.csv "1,0, 2"
+python ../../../src/csv_to_lexicon.py SYI.TAB.u8.csv "1,0, 2"
 [ $? -ne 0 ] && exit 1;
 
 # other minor fixups
@@ -15,9 +15,9 @@ perl -i -pe 's#<hw>\((.*)\)</hw>#<hw>\1</hw>#;' *.data.xml
 perl -i -pe 's#<hw>(.*?), (.*?)</hw>#<hw>\1</hw>\n    <hw>\2</hw>#;' *.data.xml
 
 # run the csv converter for the correspondences
-python3 ../../../src/csv_to_re.py NYI.TOC.u8.csv NYI.correspondences.xml NYI.classes.xml
+python ../../../src/csv_to_re.py NYI.TOC.u8.csv NYI.correspondences.xml NYI.classes.xml
 [ $? -ne 0 ] && exit 1;
-python3 ../../../src/csv_to_re.py SYI.TOC.u8.csv SYI.correspondences.xml SYI.classes.xml
+python ../../../src/csv_to_re.py SYI.TOC.u8.csv SYI.correspondences.xml SYI.classes.xml
 [ $? -ne 0 ] && exit 1;
 
 exit 0
