e321597b2a793a28a5b0ae2bdeb44b7cd0f6171a
mmazaudon <mazaudon@gmail.com>
2019-08-27 17:34:27 +0200

    version of Rewww that 'makes' under windowz

--- run_make.py
+++ run_make.py
@@ -4,35 +4,40 @@ import time
 
 
 def make(project, experiment, parameters):
+    PYTHON = 'python'
     messages = []
-    try:
+    if True:
         elapsed_time = time.time()
         if project == 'ALL':
             os.chdir('..')
-            p_object = subprocess.call(['git', 'pull', '-v'])
-            p_object = subprocess.call(['make', '-w'])
+            try:
+                p_object = subprocess.call(['git', 'pull', '-v'])
+                p_object = subprocess.call(['make', '-w'])
+            except:
+                messages.append('{:.2f} seconds. Upstream run failed.'.format(elapsed_time))
             os.chdir('REwww')
+            return messages, False
         else:
             cli = [project, experiment]
             for p in 'recon fuzzy mel run'.split(' '):
                 if p in parameters and parameters[p] != '':
                     cli += [f'--{p}', f'{parameters[p]}']
             if parameters['strict'] == 'yes': cli.append('-w')
-            # REcli.py compare TGTM experiment1 experiment1 --run1 with-hand --run2 with-clics
             os.chdir(os.path.join('..', 'src'))
-            # p_object = subprocess.call(['git', 'pull', '-v'])
-            messages.append(' '.join(['python3', 'REcli.py', 'upstream',] + cli))
-            p_object = subprocess.call(['python3', 'REcli.py', 'upstream'] + cli)
+            try:
+                # p_object = subprocess.call(['git', 'pull', '-v'])
+                messages.append(' '.join([PYTHON, 'REcli.py', 'upstream',] + cli))
+                p_object = subprocess.call([PYTHON, 'REcli.py', 'upstream'] + cli)
+            except:
+                pass
             os.chdir(os.path.join('..', 'REwww'))
         elapsed_time = time.time() - elapsed_time
         if p_object == 0:
             messages.append('{:.2f} seconds. (Re)run Upstream succeeded.'.format(elapsed_time))
             return messages, True
         else:
-            raise
-    except:
-        messages.append('{:.2f} seconds. Upstream run failed.'.format(elapsed_time))
-        return messages, False
+            messages.append('{:.2f} seconds. Upstream run failed.'.format(elapsed_time))
+            return messages, False
 
 def compare(project, experiment1, experiment2, run1, run2):
     messages = []
