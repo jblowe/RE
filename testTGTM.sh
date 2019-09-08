15fd17490a4d8bb0e57978915535417fa6d4bf27
John Lowe <jblowe@berkeley.edu>
2019-09-08 10:41:38 -0700

    testTGTM.sh now does a bit less

--- testTGTM.sh
+++ testTGTM.sh
@@ -28,13 +28,19 @@ then
    [ $? -eq 0 ] || exit 0;
 fi
 
-
 # for mel in hand wordnet clics none
 # wordnet version is too slow for regular testing use (8 mins) ... run by hand if needed.
 # first make run 'none' with no MEL
 time python3 REcli.py upstream ${PROJECT} ${EXPERIMENT} --run none --recon standard -z fuzzy > /dev/null
 [ $? -ne 0 ] && exit 1;
-for mel in hand clics wordnet
+# next make the "strict" sets: remove untouched mels and merge sets with identical support
+time python3 REcli.py upstream ${PROJECT} ${EXPERIMENT} -w --run hand-strict --mel hand --recon standard -z fuzzy > /dev/null
+[ $? -ne 0 ] && exit 1;
+# compare hand-strict to none
+time python3 REcli.py compare ${PROJECT} ${EXPERIMENT} ${EXPERIMENT} --run1 hand-strict --run2 none > ../experiments/${PROJECT}/${EXPERIMENT}/${PROJECT}.${mel}.compare.txt
+
+for mel in hand wordnet
+#for mel in hand clics wordnet
 do
     if [ -e ../experiments/${PROJECT}/${EXPERIMENT}/${PROJECT}.${mel}.mel.xml ]
     then
@@ -43,8 +49,8 @@ do
         [ $? -ne 0 ] && exit 1;
 
         # next make the "strict" sets: remove untouched mels and merge sets with identical support
-        time python3 REcli.py upstream ${PROJECT} ${EXPERIMENT} -w --run ${mel}-strict --mel ${mel} --recon standard -z fuzzy > /dev/null
-        [ $? -ne 0 ] && exit 1;
+        # time python3 REcli.py upstream ${PROJECT} ${EXPERIMENT} -w --run ${mel}-strict --mel ${mel} --recon standard -z fuzzy > /dev/null
+        # [ $? -ne 0 ] && exit 1;
 
         # coverage
         time python3 REcli.py coverage ${PROJECT} ${EXPERIMENT} ${mel}  > ../experiments/${PROJECT}/${EXPERIMENT}/${PROJECT}.${mel}.coverage.txt
@@ -52,12 +58,6 @@ do
         # compare
         time python3 REcli.py compare ${PROJECT} ${EXPERIMENT} ${EXPERIMENT} --run1 hand-strict --run2 ${mel} > ../experiments/${PROJECT}/${EXPERIMENT}/${PROJECT}.${mel}.compare.txt
 
-        # compare to non
-        time python3 REcli.py compare ${PROJECT} ${EXPERIMENT} ${EXPERIMENT} --run1 ${mel} --run2 none > ../experiments/${PROJECT}/${EXPERIMENT}/${PROJECT}.${mel}.compare.txt
-
     fi
-
-    # compare hand-strict to none
-    time python3 REcli.py compare ${PROJECT} ${EXPERIMENT} ${EXPERIMENT} --run1 hand-strict --run2 none > ../experiments/${PROJECT}/${EXPERIMENT}/${PROJECT}.${mel}.compare.txt
 
 done
