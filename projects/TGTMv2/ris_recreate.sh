20c1c2627b438d3abb2341bf22a7479ef7d9b90a
John Lowe <johnblowe@gmail.com>
2023-08-17 04:38:43 -0700

    remove TGTMv2 'test project'

--- projects/TGTMv2/ris_recreate.sh
+++
@@ -1,8 +0,0 @@
-perl -ne 'print if /^(\.+hwX?|dff|dfe|so)\b/' dico2019.txt > ALLRIS2019.DAT
-python3 tr.py ALLRIS2019.DAT A
-mv A ALLRIS2019.DAT 
-# 
-# these are the two pipeline steps for this lexicon...
-# perl Lex2XML.pl A TEMP.ris.data.xml ris.xml.log ris
-# python3 ../../src/xsltproc.py ../../styles/fmtLex.xsl TEMP.ris.data.xml > TGTM.ris.data.xml 2> ris.xslt.log
-
--- projects/TGTMv2/tangbe_split.pl
+++
@@ -1,7 +0,0 @@
-$T="⁰¹²³⁴⁵⁶'";
-$V="aeioøuɔəɛɨʌʔʱː̃";
-$C="bcdɖghjklmnŋɲpqrsʃʈtvwxyz";
-
-/([⁰¹²³⁴⁵⁶']+)([bcdɖghjklmnŋɲpqrsʃʈtvwxyz]+)?([aeioøuɔəɛɨʌʔʱː̃]+)([bcdɖghjklmnŋɲpqrsʃʈtvwxyz]+)/ && print "syll\t$1\t$2\t$3\t$4\n";
-# s/\b$T+$C+$V+$C/\1\t\2\t\3\t/;
-
