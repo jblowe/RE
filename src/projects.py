6f360916cac14d06bfea25683c854b39c1b02f03
John Lowe <johnblowe@gmail.com>
2023-08-25 05:51:10 -0700

    correct typo

--- src/projects.py
+++ src/projects.py
@@ -4,7 +4,7 @@ import os
 def get_dirs(root):
     directories = {}
     base_dir = os.path.join('..', root)
-    # if a directory has a ".master.parameters.xml" file in it, it as a corpus
+    # if a directory has a ".master.parameters.xml" file in it, it is a corpus
     for root, dirs, files in os.walk(base_dir):
         for d in sorted(dirs):
             if os.path.isfile(os.path.join(base_dir, d, f'{d}.master.parameters.xml')):
