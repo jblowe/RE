fd5a0c48d290188ee4757250bd20e4d75dcda62c
John Lowe <johnblowe@gmail.com>
2025-09-29 08:02:21 -0700

    further revisions to windows regtk/rewww install script

--- src/install.sh
+++ src/install.sh
@@ -1,11 +1,17 @@
 
-#
-pacman -S --needed mingw-w64-x86_64-python
-pacman -S --needed mingw-w64-x86_64-gcc
-pacman -S --needed mingw-w64-x86_64-glib2
-pacman -S --needed mingw-w64-x86_64-gobject-introspection
-pacman -S --needed mingw-w64-x86_64-gobject-introspection-runtime
-pacman -S --needed mingw-w64-x86_64-gtk3
-pacman -S --needed mingw-w64-x86_64-libffi
-pacman -S --needed mingw-w64-x86_64-python-cairo
-pacman -S --needed mingw-w64-x86_64-python-matplotlib
+# needed for REgtk
+pacman -S --needed --noconfirm mingw-w64-x86_64-python
+pacman -S --needed --noconfirm mingw-w64-x86_64-gcc
+pacman -S --needed --noconfirm mingw-w64-x86_64-glib2
+pacman -S --needed --noconfirm mingw-w64-x86_64-python3-gobject
+pacman -S --needed --noconfirm mingw-w64-x86_64-gobject-introspection
+pacman -S --needed --noconfirm mingw-w64-x86_64-gobject-introspection-runtime
+pacman -S --needed --noconfirm mingw-w64-x86_64-gtk3
+pacman -S --needed --noconfirm mingw-w64-x86_64-libffi
+pacman -S --needed --noconfirm mingw-w64-x86_64-python-cairo
+pacman -S --needed --noconfirm mingw-w64-x86_64-python-matplotlib
+pacman -S --needed --noconfirm mingw-w64-x86_64-python-regex
+pacman -S --needed --noconfirm mingw-w64-x86_64-python-lxml
+
+# additional module needed for REwww
+pacman -S --needed --noconfirm mingw-w64-x86_64-python-bottle
