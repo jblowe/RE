c99df431e9614eccdb8d61fea018010c4ee76bb7
John Lowe <johnblowe@gmail.com>
2025-10-10 09:40:59 -0700

    fixes to hook and pipeline for a couple projects so they work with REgtk, etc.

--- projects/DIS/hook.py
+++ projects/DIS/hook.py
@@ -45,7 +45,7 @@ def fuzzy_lexicons(mapping, settings):
 
 def generate_xml_data():
     print('running pipeline to generate data files')
-    code_dir = os.path.join('..', '..', '..', 'src')
+    code_dir = os.path.join('..', '..', 'src')
     with cd(os.path.join(base_dir)):
         with open('pipeline.sh', 'r', encoding='utf-8') as commands:
             for command in commands:
--- projects/HMONGMIEN/pipeline.sh
+++ projects/HMONGMIEN/pipeline.sh
@@ -8,11 +8,11 @@ set -x
 #python3 hm-extract.py hm-all.csv HM.TAB.csv HM.TOC.csv
 
 # run the csv converter for the data
-python3 ../../../src/csv_to_lexicon.py HM.TAB.csv "1,0, 2"
+python3 ../../src/csv_to_lexicon.py HM.TAB.csv "1,0, 2"
 [ $? -ne 0 ] && exit 1;
 
 # run the csv converter for the correspondences
-python3 ../../../src/csv_to_re.py HM.TOC.csv HM.standard.correspondences.xml HM.classes.xml
+python3 ../../src/csv_to_re.py HM.TOC.csv HM.standard.correspondences.xml HM.classes.xml
 [ $? -ne 0 ] && exit 1;
 
 exit 0
