d7a6f1a9e149bcc73f6cf311ef6fb23ef85f551d
John B. Lowe <jblowe@berkeley.edu>
2026-03-24 18:32:12 -0700

    update comment only; no code change

--- projects/HMONGMIEN/pipeline.sh
+++ projects/HMONGMIEN/pipeline.sh
@@ -4,8 +4,8 @@ set -x
 
 # do the 'initial' extraction
 
-# don't do this for now: the ToC and tabular data are now committed in GitHub for development
-#python3 hm-extract.py hm-all.csv HM.TAB.csv HM.TOC.csv
+# we don't to do this for now: the ToC and tabular data are now committed in GitHub for development
+# python3 hm-extract.py hm-all.csv HM.TAB.csv HM.TOC.csv
 
 # run the csv converter for the data
 python3 ../../src/csv_to_lexicon.py HM.TAB.csv "1,0, 2" HMONGMIEN
